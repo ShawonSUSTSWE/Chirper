e070e99803c005f4c9420fa2fea5f8363ba0f53f
ShawonSUSTSWE <53114714+ShawonSUSTSWE@users.noreply.github.com>
2021-06-24 13:04:51 +0600

    Added a Splashscreen

---
+++ app/src/main/java/com/example/chirper/Splashscreen.java
@@ -0,0 +1,43 @@
+package com.example.chirper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        getSupportActionBar().hide();
+
+        Thread thread = new Thread() {
+
+            public void run() {
+
+                try {
+
+                    sleep(4000);
+
+                } catch (Exception e) {
+
+                    e.printStackTrace();
+
+                }
+                finally {
+                    Intent intent = new Intent(Splashscreen.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+
+
+            }
+
+
+        };
+        thread.start();
+    }
+}
