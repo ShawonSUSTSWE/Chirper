186455568c577f49d4bcd9db9a4d69d4930ca41b
Nafiz132 <nafizurrahman132@gmail.com>
2021-07-25 13:57:40 +0600

    SettingsActivity

---
+++ app/src/main/java/com/example/chirper/Models/Profile/Profile02Activity.java
@@ -0,0 +1,16 @@
+package com.example.chirper.Models.Profile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.chirper.R;
+
+public class Profile02Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile02);
+    }
+}
